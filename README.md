533bb0bdbedfe74b517cbba75abf43d7f28aad51
Chai Hooli <chai.hooli@origin.com.au>
2020-10-27 15:47:33 +1100

    Code formatting using Prettier
    Code formatted & added code-comments to make it easier for later reference

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# This program is developed as part of code-challenge named "Selected Players"
+## This program is developed as part of code-challenge named "Selected Players"
 
-## Code Challenge1 : This is a program that takes the list of scores and the highest selection score as the input and gives you the number of selected player as the output.
+###### Code Challenge1 : This is a program that takes the list of scores and the highest selection score as the input and gives you the number of selected player as the output.
+
+**To run the program: Open `index.html` in the browser**
 
 ###### Structure of files
 
@@ -14,7 +16,5 @@
 2. Visual Studio Code 1.50.1
 3. Google chrome 87.0
 4. Using live-server to reflect the code-changes in real time, installation instructions here : https://gist.github.com/donmccurdy/20fb112949324c92c5e8
-
-**To run the program: Open index.html in the browser**
 
 © 2020 Chai Hooli
--- script.js
+++ script.js
@@ -1,92 +1,102 @@
-function displayScores(){
-  let enteredListOfScores = document.getElementById("inputList").value;
-  enteredListOfScores = enteredListOfScores.trim().split(' ');
+function displayScores() {
+  /**
+   * Function called when SUBMIT CTA is clicked
+   */
 
-  let highestSelectionScore = document.getElementById("selectionScore").value;  
-   
+  let enteredListOfScores = document.getElementById("inputList").value;
+  enteredListOfScores = enteredListOfScores.trim().split(" ");
+
+  let highestSelectionScore = document.getElementById("selectionScore").value;
 
   console.log(enteredListOfScores);
   let i = 0;
   let numArr = [];
   let apphaArr = [];
   let patt = /[0-9]/g;
-  let ssAlf = false;
-  for(i = 0; i<enteredListOfScores.length ; i++){
-    if(enteredListOfScores[i].match(patt)){      
+
+  //Below logic checks if entered input is alphabets & ignores them
+  for (i = 0; i < enteredListOfScores.length; i++) {
+    if (enteredListOfScores[i].match(patt)) {
       numArr.push(enteredListOfScores[i]);
-    }else{
+    } else {
       apphaArr.push(enteredListOfScores[i]);
-      document.getElementById("alphabetEntered").innerHTML = "<br>Entered list has alphabets, we have ignored them : "+apphaArr ;
-    }    
+      document.getElementById("alphabetEntered").innerHTML =
+        "<br>Entered list has alphabets, we have ignored them : " + apphaArr;
+    }
   }
 
-    // if(highestSelectionScore.match(patt)){      
-    //   highestSelectionScore = parseInt(highestSelectionScore);
-    // }else{
-    //   ssAlf = true;
-    //   document.getElementById("alphabetSelectionScore").innerHTML = "<br>Entered Selection Score is an alphabet, we have ignored it"
-    // }    
-  
-
-
-
-
-  if(enteredListOfScores == "" && highestSelectionScore == ""){
-    document.getElementById("alphabetEntered").innerHTML = ""
-    document.getElementById("displayScoreList").innerHTML = "<br>Your score list : NOT ENTERED" ;
-    document.getElementById("displaySelectionScore").innerHTML = "Highest selection score you entered :  NOT ENTERED";
-  }
-  else if(enteredListOfScores == "" && highestSelectionScore != ""){
-    document.getElementById("alphabetEntered").innerHTML = ""
-    document.getElementById("displayScoreList").innerHTML = "<br>Your score list : NOT ENTERED" ;
-    document.getElementById("displaySelectionScore").innerHTML = "Highest selection score you entered : " + highestSelectionScore;
-  }
-  else if(enteredListOfScores != "" && highestSelectionScore == ""){
-    document.getElementById("alphabetEntered").innerHTML = ""
-    document.getElementById("displayScoreList").innerHTML = "<br>List has " +numArr.length + " entries : "+ numArr ;
-    document.getElementById("displaySelectionScore").innerHTML = "Highest selection score you entered :  NOT ENTERED";
-  }
-  else{
-    document.getElementById("displayScoreList").innerHTML = "<br>List has " +numArr.length + " entries : "+ numArr ;
-  document.getElementById("displaySelectionScore").innerHTML = "Highest selection score you entered : " + highestSelectionScore;
+  //Below logic checks if no input is entered in both boxes or either of them
+  if (enteredListOfScores == "" && highestSelectionScore == "") {
+    document.getElementById("alphabetEntered").innerHTML = "";
+    document.getElementById("displayScoreList").innerHTML =
+      "<br>Your score list : NOT ENTERED";
+    document.getElementById("displaySelectionScore").innerHTML =
+      "Highest selection score you entered :  NOT ENTERED";
+  } else if (enteredListOfScores == "" && highestSelectionScore != "") {
+    document.getElementById("alphabetEntered").innerHTML = "";
+    document.getElementById("displayScoreList").innerHTML =
+      "<br>Your score list : NOT ENTERED";
+    document.getElementById("displaySelectionScore").innerHTML =
+      "Highest selection score you entered : " + highestSelectionScore;
+  } else if (enteredListOfScores != "" && highestSelectionScore == "") {
+    document.getElementById("alphabetEntered").innerHTML = "";
+    document.getElementById("displayScoreList").innerHTML =
+      "<br>List has " + numArr.length + " entries : " + numArr;
+    document.getElementById("displaySelectionScore").innerHTML =
+      "Highest selection score you entered :  NOT ENTERED";
+  } else {
+    document.getElementById("displayScoreList").innerHTML =
+      "<br>List has " + numArr.length + " entries : " + numArr;
+    document.getElementById("displaySelectionScore").innerHTML =
+      "Highest selection score you entered : " + highestSelectionScore;
   }
 
-  //Call to function where entered numbers are sorted & final list is calculated
-  selectPlayers(numArr,highestSelectionScore);
+  //Call to function to sort & prepare the final list
+  selectPlayers(numArr, highestSelectionScore);
 }
 
+function selectPlayers(calcArr, selector) {
+  /**
+   * @param calcArr           Array of input to sort
+   * @param selector          number to match when calculating final list
+   */
 
-
-
-
-function selectPlayers(calcArr,selector) {
-  
   console.log(calcArr);
   //sorting into descending order
-  calcArr = calcArr.sort(function(a, b){return b-a});  
+  calcArr = calcArr.sort(function (a, b) {
+    return b - a;
+  });
 
   console.log("calArr: " + calcArr);
   selector = parseInt(selector);
-  console.log("selector: " + selector);
-
   let m = 0;
   let finalList = [];
   let flag = false;
-  for(m=0; m<calcArr.length; m++){    
-    if(calcArr[m] >= selector){
-      finalList.push(calcArr[m]);        
-    }else{
+
+  // logic to push the sorted & selected array of scores to a new array
+  for (m = 0; m < calcArr.length; m++) {
+    if (calcArr[m] >= selector) {
+      finalList.push(calcArr[m]);
+    } else {
       flag = true;
-    }    
+    }
   }
 
-  if(flag == true && finalList != ""){
-    document.getElementById("result").innerHTML  = "<br>Number of selected players is " + finalList.length+", and their score is: " + finalList;      
-  }else if(finalList != ""){
-    document.getElementById("result").innerHTML  = "<br>Number of selected players is " + finalList.length+", and their score is: " + finalList;      
-  }else{
-    document.getElementById("result").innerHTML  = "<br>No players selected ";  
+  //logic to display final selected array on the UI
+  if (flag == true && finalList != "") {
+    document.getElementById("result").innerHTML =
+      "<br>Number of selected players is " +
+      finalList.length +
+      ", and their score is: " +
+      finalList;
+  } else if (finalList != "") {
+    document.getElementById("result").innerHTML =
+      "<br>Number of selected players is " +
+      finalList.length +
+      ", and their score is: " +
+      finalList;
+  } else {
+    document.getElementById("result").innerHTML = "<br>No players selected ";
   }
-  console.log("FinalList: " +finalList);
- 
+  console.log("FinalList: " + finalList);
 }
